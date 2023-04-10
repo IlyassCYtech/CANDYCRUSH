2dfa76a27eca3952c039d0fa25953f982d0f033b
IlyassCYtech <130382885+IlyassCYtech@users.noreply.github.com>
2023-04-10 20:13:12 +0200

    Update Executeur.c

--- Executeur.c
+++ Executeur.c
@@ -1,3 +1,12 @@
 
 
-
+void executeur(int tab[size][size]) {
+    int r;
+  printf("quelle est la ligne de la premiere case");
+  
+  int temp = tab[i1][j1];
+    tab[i1][j1] = tab[i2][j2];
+    tab[i2][j2] = temp;
+  r= premier_vérificateur(int tab);
+  while(r=0)
+}
