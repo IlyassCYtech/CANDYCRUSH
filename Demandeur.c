ad31113bf9db559372eabf11be1b5c81735830f2
IlyassCYtech <130382885+IlyassCYtech@users.noreply.github.com>
2023-04-11 22:22:43 +0200

    Create Demandeur.c

---
+++ Demandeur.c
@@ -0,0 +1,9 @@
+void demandeur(struct *p, struct *c){
+printf("QUelle sont les coordonnée du point en largeur.");
+  scanf("%d",p.longitude);
+printf("QUelle sont les coordonnée du point en longueur.");
+  scanf("%d",p.longitude);
+printf("QUelle sont les coordonnée du point en largeur du point 2.");
+  scanf("%d",p.longitude);
+printf("QUelle sont les coordonnée du point en largeur du point 2");
+  scanf("%d",p.longitude);
