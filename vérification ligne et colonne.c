d8d85d5a72094cd038f28f3c6c5c7bac61080df2
IlyassCYtech <130382885+IlyassCYtech@users.noreply.github.com>
2023-04-11 23:13:25 +0200

    Update vérification ligne et colonne.c

--- vérification ligne et colonne.c
+++ vérification ligne et colonne.c
@@ -1,7 +1,7 @@
 int vérificateurcontinu(int tab, int *r, int *v) {
     int valeur = 0; 
     
-   
+   //ligne 
     for (int i = 0; i < taille; i++) {
         for (int j = 0; j < taille-2; j++) {
             if (tab[i][j] == tab[i][j+1] && tab[i][j] == tab[i][j+2]) {
@@ -12,7 +12,7 @@ int vérificateurcontinu(int tab, int *r, int *v) {
         }
     }
     
-   
+   //colonne
     for (int j = 0; j < taille; j++) {
         for (int i = 0; i < taille-2; i++) {
             if (tab[i][j] == tab[i+1][j] && tab[i][j] == tab[i+2][j]) {
