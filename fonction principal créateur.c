a5e1ff80caa01a3cfbcce8fad1919b7e096133e1
IlyassCYtech <130382885+IlyassCYtech@users.noreply.github.com>
2023-04-11 22:24:22 +0200

    Update fonction principal créateur.c

--- fonction principal créateur.c
+++ fonction principal créateur.c
@@ -4,8 +4,8 @@
 
 void create_tableau(int tab[size][size],number) {
     srand(time(NULL)); 
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
             tab[i][j] = rand() % number; 
         }
     }
