55dfe6e4f885720ae775d1e3f107df461859f732
IlyassCYtech <130382885+IlyassCYtech@users.noreply.github.com>
2023-04-12 13:32:12 +0200

    Update Demandeur.c

--- Demandeur.c
+++ Demandeur.c
@@ -1,9 +1,22 @@
-void demandeur(struct *p, struct *c){
-printf("QUelle sont les coordonnée du point en largeur.");
-  scanf("%d",p.longitude);
-printf("QUelle sont les coordonnée du point en longueur.");
-  scanf("%d",p.longitude);
-printf("QUelle sont les coordonnée du point en largeur du point 2.");
-  scanf("%d",p.longitude);
-printf("QUelle sont les coordonnée du point en largeur du point 2");
-  scanf("%d",p.longitude);
+
+void demandeur(struct point *p, struct point *c, size_t size) {
+    do {
+        printf("Quelles sont les coordonnées du point en longueur ? ");
+        scanf("%f", &(p->longueur));
+    } while (p->longueur < 0 || p->longueur >= size || p->longueur != (int)p->longueur);
+
+    do {
+        printf("Quelles sont les coordonnées du point en largeur ? ");
+        scanf("%f", &(p->largeur));
+    } while (p->largeur < 0 || p->largeur >= size || p->largeur != (int)p->largeur);
+
+    do {
+        printf("Quelles sont les coordonnées du point 2 en longueur ? ");
+        scanf("%f", &(c->longueur));
+    } while (c->longueur < 0 || c->longueur >= size || c->longueur != (int)c->longueur);
+
+    do {
+        printf("Quelles sont les coordonnées du point 2 en largeur ? ");
+        scanf("%f", &(c->largeur));
+    } while (c->largeur < 0 || c->largeur >= size || c->largeur != (int)c->largeur);
+}
