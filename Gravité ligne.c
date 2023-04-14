1e24b46c6d5ff59a2e1897a593d335d1f2675c91
Nidhal-dev <65866667+Nidhal-dev@users.noreply.github.com>
2023-04-14 10:32:34 +0200

    Update Gravité ligne.c

--- Gravité ligne.c
+++ Gravité ligne.c
@@ -1,2 +1,41 @@
 include <stdio.h>
-include 
+
+void main(){
+
+int count=0;
+int stock=0;
+int rep=0;
+
+for (int i = 0; i < size; i++) {
+  for (int j = 0; j < size; j++) {
+    if(tab[i][j]==0&&tab[i+1][j]==0){
+      rep=j;
+    }
+  }
+}
+
+for (int i = 0; i < size; i++) {
+  for (int j = 0; j < size; j++) {
+    if(tab[i][j]==0&&tab[i+1][j]==0){
+      rep=j;
+    }
+  }
+}
+
+if(rep=!1){
+  for(int z=0;z<rep-1;z++){
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        if(tab[i][j]==0&&tab[i+1][j]==0){
+          while(tab[i][j]==0){
+            count++;
+          }
+          for(int w=0;w<count;w++){
+            stock=tab[i+w][j-1];
+            tab[i+w][j-1]=0;
+            tab[i+w][j]=stock;
+          }
+        }
+        
+        
+          
