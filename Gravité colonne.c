127e5ce114635216846ce4c0e8b50e3a0d47a124
Nathan <123404137+NatixLaGaule@users.noreply.github.com>
2023-04-14 18:28:35 +0200

    Update Gravité colonne.c

--- Gravité colonne.c
+++ Gravité colonne.c
@@ -4,13 +4,12 @@
 
 void graviteColonne(int tab, int *r, int *v, int number){
     srand(time(NULL));
-	*r+=3;
-	int j=*v;
-	int i=*r;
+	int j=v;
+	int i=r+2;
 	for(i, j; i>2 ; i--){
 		tab[i][j]=tab[i-3][j];
     }
-    for(int d=0, j; d<3 ; i++){
-        tab[d][j]=rand()% number;
-    }
+    	for(int d=0, j; d<3 ; i++){
+        	tab[d][j]=rand()% number;
+    	}
 }
